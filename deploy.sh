4a54d377c38ae6d779705a4a56c33fc3b2afd4fa
GarethChester <gareth.chester@dunelm.org.uk>
2021-07-04 11:32:47 +0100

    Add deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -env
+
+# build
+
+# navigate into the build output directory
+
+cd dist/spa
+
+git init
+
+git add -A
+
+git commit -m 'deploy'
+
+git push -f git@github.com:GarethChester/recipes-server-251.git master:gh-ages
+
+cd -
