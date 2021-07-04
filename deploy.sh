9db56a93b5fe4f916ff82ddd99d5b988ad5ae15e
GarethChester <gareth.chester@dunelm.org.uk>
2021-07-04 11:53:33 +0100

    Remove init from git deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env sh
 
 # abort on errors
-set -env
+set -e
 
 # build
-
 quasar build
 
 # navigate into the build output directory
-
 cd dist/spa
 
-git init
-
-git add -A
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 
 git commit -m 'deploy'
 
-git push -f git@github.com:GarethChester/recipes-server-251.git master:gh-pages
+git push -f git@github.com:username/hello-world.git master:gh-pages
 
 cd -
