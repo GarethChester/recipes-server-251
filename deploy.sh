d25b7bfee768865f35eb23b56d77034f93b6cdbc
GarethChester <gareth.chester@dunelm.org.uk>
2021-07-04 23:40:08 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,12 @@ cd dist/spa
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+git init
+
+git add -A
+
 git commit -m 'deploy'
 
-git push -f git@github.com:username/hello-world.git master:gh-pages
+git push -f git@github.com:GarethChester/recipes-server-251.git master:gh-pages
 
 cd -
